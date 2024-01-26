18595dcd97f79334c7a34d5b419dcb01e8b26a36
bydequ <77545598+bydequ@users.noreply.github.com>
2024-01-26 14:32:22 +0300

    Update build.gradle.kts
    v.1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0")
 
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.compose.material:material:1.5.4")
